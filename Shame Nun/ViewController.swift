72840204c3609b17c18ced823e9ba0a12b69ee2c
Johannes Gorset <jgorset@gmail.com>
2015-07-11 23:02:00 +0200

    Initial Commit

---
+++ Shame Nun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shame Nun
+//
+//  Created by Johannes on 11/07/15.
+//  Copyright (c) 2015 Johannes Gorset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
